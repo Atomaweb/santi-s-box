dec73f4a5090415738be18fc88862c01860704c5
Atomaweb <contacto@atoma.cl>
2026-08-25 09:54:10 -0400

    Add RealName to strange and Unfree modes

--- synth/SynthConfig.ts
+++ synth/SynthConfig.ts
@@ -230,8 +230,8 @@ export class Config {
 		{name: "Locrian",          RealName: Askrian,           flags: [true, true,  false,  true, false,  true, true,  false,  true, false,  true, false]},
 		{name: "Lydian Dominant", RealName: Dissonance,         flags: [true,  false, true, false,  true,  false, true,  true,  false, true, true,  false]},
 		{name: "Phrygian Dominant", RealName: Ressonant,        flags: [true, true,  false,  false, true, true,  false,  true,  true, false, true,  false]},
-		{name: "strange",            flags: [true, false,  true, false,  true, false,  true, false,  true, false,  true, false]},
-		{name: "Unfree",             flags: [true,  true,  true,  false,  true,  true,  true,  true,  true,  true,  true,  true]},
+		{name: "strange",            RealName: Whole Tone,      flags: [true, false,  true, false,  true, false,  true, false,  true, false,  true, false]},
+		{name: "Unfree",             RealName: Unchromatic,     flags: [true,  true,  true,  false,  true,  true,  true,  true,  true,  true,  true,  true]},
 	]);
 	public static readonly keys: DictionaryArray<Key> = toNameMap([
 		{name: "C",  isWhiteKey:  true, basePitch: 12}, // C0 has index 12 on the MIDI scale. C7 is 96, and C9 is 120. C10 is barely in the audible range.
